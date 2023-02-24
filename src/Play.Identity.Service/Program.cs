410e5cc643e906aa2c60ab4d14004a1eee4749b1
Wagner Bolfe <shaper@live.com>
2023-02-24 11:16:23 -0300

    Added ready and live checks

--- src/Play.Identity.Service/Program.cs
+++ src/Play.Identity.Service/Program.cs
@@ -3,17 +3,21 @@ using System.IO;
 using System.Reflection;
 using GreenPipes;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using Play.Common.MassTransit;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
 using Play.Identity.Service.Exceptions;
+using Play.Identity.Service.HealthChecks;
 using Play.Identity.Service.HostedServices;
 using Play.Identity.Service.Settings;
 
@@ -59,7 +63,20 @@ builder.Services.AddControllers();
 builder.Services.AddHostedService<IdentitySeedHostedService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHealthChecks();
+
+builder.Services.AddHealthChecks()
+    .Add(new HealthCheckRegistration
+        (
+            "mongodb",
+            serviceProvider => 
+            {
+                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+                return new MongoDbHealthCheck(mongoClient);
+            },
+            HealthStatus.Unhealthy,
+            new[] { "ready" },
+            TimeSpan.FromSeconds(3)
+        ));
 
 var app = builder.Build();
 
@@ -89,6 +106,13 @@ app.UseCookiePolicy(new CookiePolicyOptions
 
 app.MapControllers();
 app.MapRazorPages();
-app.MapHealthChecks("/health");
+app.MapHealthChecks("/health/ready", new HealthCheckOptions()
+{
+    Predicate = (check) => check.Tags.Contains("ready")
+});
+app.MapHealthChecks("/health/live", new HealthCheckOptions()
+{
+    Predicate = (check) => false
+});
 
 app.Run();
